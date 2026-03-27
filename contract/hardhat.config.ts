bf4f903ecf0c7b1317c84e5bbec99f33c31dd74b
longdevbf <longtda5k48gtb@gmail.com>
2026-03-27 11:41:15 +0700

    up

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,5 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
+import "@oasisprotocol/sapphire-hardhat";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -7,6 +9,31 @@ const config: HardhatUserConfig = {
     settings: {
       evmVersion: "cancun",
     },
+  },
+  networks: {
+    sapphireTestnet: {
+      url: "https://testnet.sapphire.oasis.dev",
+      chainId: 23295,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sapphireTestnet: "sapphire",
+    },
+    customChains: [
+      {
+        network: "sapphireTestnet",
+        chainId: 23295,
+        urls: {
+          apiURL: "https://testnet.explorer.sapphire.oasis.dev/api",
+          browserURL: "https://testnet.explorer.sapphire.oasis.dev",
+        },
+      },
+    ],
+  },
+  sourcify: {
+    enabled: true,
   },
 };
 
